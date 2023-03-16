cadeb609a972bc8d4ec1a432181c46fe9d5d5275
rps5057 <rps5057@gmail.com>
2023-03-16 16:25:11 +0530

    removed the redundant nuvometal_db folder

--- nuvometal_db/material_search/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import PropertiesList, MaterialProperties, PropertySearch
-
-admin.site.register(PropertiesList)
-admin.site.register(MaterialProperties)
-admin.site.register(PropertySearch)
--- nuvometal_db/material_search/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class MaterialSearchConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'material_search'
